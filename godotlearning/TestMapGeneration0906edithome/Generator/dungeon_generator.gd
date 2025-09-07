7c4d3a19184f6dacc63096e789a277618bac6b17
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2025-09-07 22:55:04 +0800

    0907edithome
    *修复相邻房间问题

--- godotlearning/TestMapGeneration0906edithome/Generator/dungeon_generator.gd
+++ godotlearning/TestMapGeneration0906edithome/Generator/dungeon_generator.gd
@@ -31,7 +31,7 @@ var enemy_pools := {
 
 @export var width := 100
 @export var height := 100
-@export var min_rooms := 10
+@export var min_rooms := 10 
 @export var max_rooms := 18
 @export var min_path_len := 6
 @export var rng_seed := 123456 #TODO 这个种子似乎没有起作用 
@@ -79,7 +79,7 @@ func _generate_layout():
 			if not grid.has(nxt):
 				_create_room(nxt)
 				_connect_rooms(pos, nxt) # 分支必连
-				#_maybe_connect_to_other_neighbors(pos) # 有概率额外连门
+				_maybe_connect_to_other_neighbors(pos) # 有概率额外连门
 				pos = nxt
 			else :
 				break
@@ -89,11 +89,32 @@ func _generate_layout():
 		#r.neighbors = _find_neighbors(r.grid_pos)
 		r.difficulty = r.grid_pos.distance_to(start_pos)
 
+#创建房间 使用默认值 
+func _create_room(pos: Vector2i, t := RoomData.RoomType.NORMAL):
+	var rd := RoomData.new()
+	rd.id = rooms.size()
+	rd.grid_pos = pos
+	rd.type = t
+	rooms.append(rd)
+	grid[pos] = rd.id
+
+ #随机寻找房间 
+func _random_neighbor_step(pos: Vector2i, avoid_backtrack := true) -> Vector2i:
+	var dirs := [Vector2i(0,-1), Vector2i(1,0), Vector2i(0,1), Vector2i(-1,0)] #TODO 主线房间最好有一个大方向 
+	dirs.shuffle()
+	for d in dirs:
+		var np = pos + d
+		if abs(np.x - start_pos.x) > width/2: continue #这段代码保证房间不会超出边界
+		if abs(np.y - start_pos.y) > height/2: continue
+		if avoid_backtrack and grid.has(np): continue
+		return np
+	# 兜底：随机方向
+	return pos + dirs[0]
+
 # 连接两个房间（双向）
 func _connect_rooms(a: Vector2i, b: Vector2i) -> void:
 	var room_a = grid[a]
 	var room_b = grid[b]
-
 
 	if not rooms[room_a].neighbors.has(room_b):
 		rooms[room_a].neighbors.append(room_b)
@@ -108,31 +129,14 @@ func _maybe_connect_to_other_neighbors(pos: Vector2i, extra_prob: float = 0.25) 
 		if grid.has(np):
 			var room_a = rooms[grid[pos]]
 			var room_b = rooms[grid[np]]
-			if not room_a.neighbors.has(room_b):
+			if not room_a.neighbors.has(room_b.id):
 				if rng.randf() < extra_prob:
+				#if rng.randf() < 1:
 					_connect_rooms(pos, np)
+					print("额外连接房间")
 
 
-func _create_room(pos: Vector2i, t := RoomData.RoomType.NORMAL):
-	var rd := RoomData.new()
-	rd.id = rooms.size()
-	rd.grid_pos = pos
-	rd.type = t
-	rooms.append(rd)
-	grid[pos] = rd.id
 
-
-func _random_neighbor_step(pos: Vector2i, avoid_backtrack := true) -> Vector2i: #随机寻找房间 
-	var dirs := [Vector2i(0,-1), Vector2i(1,0), Vector2i(0,1), Vector2i(-1,0)] #TODO 主线房间最好有一个大方向 
-	dirs.shuffle()
-	for d in dirs:
-		var np = pos + d
-		if abs(np.x - start_pos.x) > width/2: continue #这段代码保证房间不会超出边界
-		if abs(np.y - start_pos.y) > height/2: continue
-		if avoid_backtrack and grid.has(np): continue
-		return np
-	# 兜底：随机方向
-	return pos + dirs[0]
 
 #寻找邻居 
 func _find_neighbors(pos: Vector2i) -> Array[int]:
@@ -150,13 +154,13 @@ func _find_neighbors(pos: Vector2i) -> Array[int]:
 	return ids
 
 
-func _assign_room_types(): 
+func _assign_room_types():
 	# 选 Boss：离起点最远的普通房
 	var boss = _find_boss_room()
 	boss.type = RoomData.RoomType.BOSS
 
 	# 选宝物房：中等距离的普通房（不与 Boss 相邻）
-	var candidates := rooms.filter(func(r):
+	var candidates := rooms.filter(func(r): #筛选出符合条件的房间
 		return r.type == RoomData.RoomType.NORMAL and r.difficulty >= min_path_len/2 and not _adjacent_to_type(r, RoomData.RoomType.BOSS))
 	if candidates.size() > 0:
 		candidates.shuffle()
@@ -168,16 +172,16 @@ func _assign_room_types():
 		shops.shuffle()
 		shops[0].type = RoomData.RoomType.SHOP
 
-	# 隐藏房：寻找“被 >=3 个房包围”的空位再回填
-	var secret_spots := _find_secret_spots(3)
-	if secret_spots.size() > 0:
-		var pos := secret_spots[0]
-		_create_room(pos, RoomData.RoomType.SECRET)
-#
-	# 超级隐藏：恰好只有 1 个邻居的空位
-	var ssecret := _find_secret_spots(1, 1)
-	if ssecret.size() > 0:
-		_create_room(ssecret[0], RoomData.RoomType.SUPER_SECRET)
+	## 隐藏房：寻找“被 >=3 个房包围”的空位再回填 #TODO 隐藏房间还未设置 connect
+	#var secret_spots := _find_secret_spots(3)
+	#if secret_spots.size() > 0:
+		#var pos := secret_spots[0]
+		#_create_room(pos, RoomData.RoomType.SECRET)
+##
+	## 超级隐藏：恰好只有 1 个邻居的空位
+	#var ssecret := _find_secret_spots(1, 1)
+	#if ssecret.size() > 0:
+		#_create_room(ssecret[0], RoomData.RoomType.SUPER_SECRET)
 
 
 func _find_boss_room() -> RoomData:
@@ -188,7 +192,7 @@ func _find_boss_room() -> RoomData:
 	return max_room
 
 
-func _adjacent_to_type(r: RoomData, t: int) -> bool:
+func _adjacent_to_type(r: RoomData, t: int) -> bool: #可以检测旁边的房间是否为指定类型 
 	for nid in r.neighbors:
 		if rooms[nid].type == t:
 			return true
@@ -220,7 +224,8 @@ func _compute_door_masks():
 		var mask := 0
 		for off in offset_to_bit.keys():
 			var np = r.grid_pos + off
-			if grid.has(np):
+			#if grid.has(np): 
+			if grid.has(np) && r.neighbors.has(grid[np]): #这里再补充一下 确认这个点确实再 r 的 
 				mask |= offset_to_bit[off] #在已知房间中寻找已经存在的房间 
 		r.door_mask = mask
 
@@ -228,6 +233,9 @@ func _compute_door_masks():
 func _instantiate_rooms():
 	var cell_size := Vector2(256, 256) # 你的房间世界尺寸
 	for r in rooms:
+		#if r.neighbors.size() == 0:
+			#return
+		#
 		var scene = room_scene_by_mask.get(r.door_mask)
 		var inst = scene.instantiate()
 		add_child(inst)
@@ -277,13 +285,13 @@ func _spawn_enemies(r: RoomData, base_count: int = 3, t : String = "普通房间
 
 func _room_node(room_id: int) -> Node2D:
 	for c in get_children():
-		if c.get_meta("room_id") == room_id:
-			return c
+		if c.has_meta("room_id"):
+			if c.get_meta("room_id") == room_id:
+				return c
 	return null
 
 
 func _random_point_in_room(room: Node2D) -> Vector2:
-	print("房间初始位置", room.global_position)
 	# 先拿房间的区域范围（这里假设房间场景的大小是 512x320）
 	var size = Vector2(128, 128)
 	
@@ -292,5 +300,4 @@ func _random_point_in_room(room: Node2D) -> Vector2:
 	var local_y = rng.randf_range(-size.y/2, size.y/2)
 
 	var pos = room.position + Vector2(local_x, local_y)
-	print("敌人位置", pos)
 	return pos
